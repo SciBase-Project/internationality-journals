efd8c4a37ba14e77e57ce9edf4aa9d0e5874f9b8
Sukrit Venkatagiri <sukrit.v.95@gmail.com>
2016-02-09 23:49:07 +0530

    Added get_journal_list_Aminer.py to get list of journals present in Elesevier and Aminer.

--- src/get_journal_list_Aminer.py
+++ src/get_journal_list_Aminer.py
@@ -2,53 +2,64 @@ __author__ = 'Sukrit'
 import csv
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
-
-file = open("../data/aminer_publications.txt")
-lines = file.readlines()
-papers = []
-i = 0
-while i < len(lines) :
-    while lines[i] !=  '  \r\n' :
-        line = lines[i].strip()
-
-        '''
-        #index ---- index id of this paper
-        #* ---- paper title
-        #@ ---- authors (separated by semicolons)
-        #o ---- affiliations (separated by semicolons, and each affiliaiton corresponds to an author in order)
-        #t ---- year
-        #c ---- publication venue
-        #% ---- the id of references of this paper (there are multiple lines, with each indicating a reference)
-        #! ---- abstract
-        '''
-        if line.startswith('#c') :     papers.append(line[len('#c'):])
+#import matplotlib.pyplot as plt
+#from scipy.optimize import curve_fit
 
 
-file.close()
-
-print "[DEBUG] Number of papers ", len(papers)
-print papers
-
-'''
-f = open('../data/journal_snip.txt', 'r') #reading list of journals present in Aminer
-x = f.readlines()
-xn = []
-for line in x:
-    line = line.replace('&','and') #converting & to 'and' [UGH]
-    xn.append(line.rstrip())
+ELElist = []
+with open('../data/Elsevier_journal_list.csv', 'r') as file :
+    x = file.readlines()
+    for line in x :
+        #print line
+        line = line.replace('&','and') #converting & to 'and' [UGH]
+        ELElist.append(line.rstrip()) #remove whitespaces
 
 
 
-SNIP = pd.read_csv("../data/journal_SNIP_values.csv")
+import pymongo
+client = pymongo.MongoClient("localhost", 27017)
+# db name - aminer
+db = client.aminer
+# collection
+db.publications
 
-
-journals = SNIP['Source Title'] #taking only 'Source Title' column
 jlist = []
 i = 0
-j = 0
 
-for jname in journals :
+for jname in ELElist :
+    if db.publications.find(filter = {'publication' : jname},limit = 1) != None :
+        jlist.append(jname)
+        print jname
+        i += 1
+print i
+
+with open ("../output/both_journal_list.txt","w")as file:
+    for line in jlist:
+        file.write(line+"\n")
 
 '''
+cursor = db.publications.find()
+for document in cursor :
+    if document['publication'] not in jlist :
+        if document['publication'] in ELElist :
+            jlist.append(document['publication'])
+            print document['publication']
+'''
+
+
+
+'''
+citable_items = list(db.publications.find({"publication" : P})
+citable_items_ids = []
+for cite in citable_items : citable_items_ids.append(cite['index'])
+'''
+
+
+#print "[DEBUG] Number of papers ", len(papers)
+#print papers
+
+
+
+
+
+
